80feca98e05678f2fd7669033379d79746eca10a
Asma <asma.hussaini@zartis.com>
2022-10-13 08:05:31 +0100

    initialize 4 integers input to indicate postion of the light and thor

--- Power of thor.js
+++ Power of thor.js
@@ -6,44 +6,5 @@ var initialTY = parseInt(inputs[3]); // Thor's starting Y position
 var thorX = initialTX;
 var thorY = initialTY;
 // game loop
-while (true) {
-    var remainingTurns = parseInt(readline());
-    var directionX = "";
-    var directionY = "";
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-    if(thorX > lightX){
-        directionX = "W";
-        thorX --;
-    }else if(thorX < lightX) {
-        directionX = "E";
-        thorX ++;
-    }else{
-        directionX = "";
-        
-    var remainingTurns = parseInt(readline());
-    var directionX = "";
-    var directionY = "";
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-    if(thorX > lightX){
-        directionX = "W";
-        thorX --;
-    }else if(thorX < lightX) {
-        directionX = "E";
-        thorX ++;
-    }else{
-        direc
-    }
-    if(thorY > lightY){
-        directionY = "N";
-        thorY --;
-    }else if(thorY < lightY){
-        directionY = "S";
-        thorY ++;
-    }else{
-        directionY = "";
-    }
-    // A single line providing the move to be made: N NE E SE S SW W or NW
-    console.log(directionY + directionX);
-}
+
+
